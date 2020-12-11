344ef07b946bf45155856a8130d0ae4be3c80599
Thomas Münch <tmuench@awi.de>
2020-12-11 14:43:32 +0100

    make some beautification.

--- R/BarometricFormula.R
+++ R/BarometricFormula.R
@@ -1,28 +1,28 @@
-##' Barometric height formula
-##'
-##' Calculate atmospheric surface pressure from elevation and temperature using
-##' the barometric height formula for an isothermal atmosphere.
-##' @param T surface temperature in degrees Celsius
-##' @param h surface elevation in [m]
-##' @param p0 reference surface pressure; defaults to 1013 mbar
-##' @return surface pressure in [mbar] at elevation \code{h} and temperature
-##' \code{T}
-##' @author Thomas Münch
-##' @examples
-##' # Surface pressure at Kohnen Station:
-##' BarometricFormula(-44.5, 2892)
-##' @export
+#' Barometric height formula
+#'
+#' Calculate atmospheric surface pressure from elevation and temperature using
+#' the barometric height formula for an isothermal atmosphere.
+#'
+#' @param T surface temperature in degree Celsius.
+#' @param h surface elevation in [m].
+#' @param p0 reference surface pressure; defaults to 1013 mbar.
+#' @return surface pressure in [mbar] at elevation \code{h} and temperature
+#'   \code{T}.
+#' @author Thomas Münch
+#' @examples
+#' # Surface pressure at Kohnen Station, East Antarctica:
+#' BarometricFormula(-44.5, 2892)
+#' @export
 BarometricFormula <- function(T, h, p0 = 1013) {
 
-    kR <- 8.314
-    kg <- 9.807
-    kM <- 0.02896
+  kR <- 8.314
+  kg <- 9.807
+  kM <- 0.02896
 
-    T <- T + 273.15
+  T <- T + 273.15
 
-    hs <- kR * T / (kg * kM)
+  hs <- kR * T / (kg * kM)
 
-    return(p0 * exp(-h / hs))
+  return(p0 * exp(-h / hs))
 
 }
-
